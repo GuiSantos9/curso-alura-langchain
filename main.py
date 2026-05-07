ec7e3fb473c5ed17488916ff1e1f38f1fddd7a25
Guilherme Augusto dos Santos <guilhermeaugusto923@gmail.com>
2026-05-07 17:11:00 -0300

    validação de segurança da api_key no arquivo .env

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import os
 
+# carrega as variáveis do arquivo .env
 load_dotenv()
+
+# acessa a chave de uma API de forma segura
 api_key = os.getenv("OPENAI_API_KEY")
+
+# verificação se a chave da API existe
+if api_key is None:
+    raise ValueError("A chave da API não foi definida no .env")
+
+print("Chave carregada com sucesso!")
 
 numero_dias = 7
 numero_criancas = 5
